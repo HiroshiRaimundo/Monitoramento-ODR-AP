66734c277372ce7e30c37bf0eaa5d5a0054029ef
Mestre do Universo <kogacheiroso@gmail.com>
2025-03-13 13:41:57 -0300

    Descrição das alterações

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  build: {
+    chunkSizeWarningLimit: 1000, // Aumenta o limite para 1000 KB
+    rollupOptions: {
+      external: ["react-syntax-highlighter/dist/esm/styles/prism"], // Adiciona aqui
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          vendor: ["lodash", "moment"],
+        },
+      },
+    },
+  },
+});
